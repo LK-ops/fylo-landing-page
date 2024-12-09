e4b02cba4a684d09214301043a57aec3e368839a
LK-ops <60146938+LK-ops@users.noreply.github.com>
2024-12-09 19:19:01 +0400

    .
    .

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
 import { Box } from './components/organisms/Box/Box'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Team from './Team'
+import Features from './Features'
+import SignIn from './SignIn'
+import Header from './components/molecules/Header/Header'
+
+
 
 function App() {
 
   return (
     <>
-     <Box/>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Box/>}/>
+        <Route path="/features" element={<Features/>}/>
+        <Route path="/team" element={<Team/>}/>
+        <Route path="/signIn" element={<SignIn/>}/>
+      </Routes>
+    </Router>
     </>
   )
 }
--- vite-project/src/components/molecules/Header/Header.tsx
+++ vite-project/src/components/molecules/Header/Header.tsx
@@ -1,13 +1,30 @@
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const goToTeam = () => {
+    navigate('/team');
+  }
+
+  const goToFeatures = () => {
+    navigate('/features');
+  }
+
+  const goToSignIn = () => {
+    navigate('/signIn');
+  }
+
   return (
     <div className=" flex  justify-between items-center w-full h-[100px] p-[80px]" >
         <div className="flex"><img className="w-[166px] h-[49px] " src="logo.svg" alt="Logo" />
         </div>
         <div className="flex w-[300px] justify-between text-[#07043B] font-raleway text-[16px]">
-        <div>Features</div>
-        <div>Team</div>
-        <div>Sign In</div>
+        <button onClick={goToFeatures}>Features</button>
+        <button onClick={goToTeam}>Team</button>
+        <button onClick={goToSignIn}>Sign In</button>
         </div>
         </div>
   )
